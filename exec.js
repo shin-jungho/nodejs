288d5cf7a1142135a79538080ebe1d5f8a450863
jungho shin <wjdghtls11@gmail.com>
2021-12-22 01:28:40 +0900

    exec prac2

--- exec.js
+++ exec.js
@@ -4,9 +4,9 @@ const exec = require('child_process').exec;
 var process = exec('dir');
 
 process.stdout.on('data', function (data) {
-  console.log(data.toString(utf8));
+  console.log(data.toString('utf8'));
 })
 
 process.stderr.on('data', function (data) {
-  console.error(data.toString(utf8));
+  console.error(data.toString('utf8'));
 })

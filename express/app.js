14cac165f266cfa5492da57f4e735680b145d7cb
jungho shin <wjdghtls11@gmail.com>
2021-12-29 18:28:31 +0900

    express method Prac

--- express/app.js
+++ express/app.js
@@ -1,22 +1,18 @@
-const dotenv = require('dotenv');
-dotenv.config(); // process.env 쓰는값보다 위에 올리기
 const express = require('express');
-const path = require('path');
-const morgan = require('morgan'); // 요청 응답 기록하는 것 
+const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const multer = require('multer');
-const indexRouter = require('./routes');
-const userRouter = require('./routes/user');
+const dotenv = require('dotenv');
+const path = require('path');
 
+dotenv.config();
 const app = express();
-
-app.set('port', process.env.PORT || 3000); //서버에다가 속성을 심는다는 느낌 (전역변수 느낌)
-
-// 미들웨어 순서 중요
+app.set('port', process.env.PORT || 3000);
 
 app.use(morgan('dev'));
-app.use('/', express.static(__dirname, 'public'));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
   resave: false,
@@ -24,27 +20,20 @@ app.use(session({
   secret: process.env.COOKIE_SECRET,
   cookie: {
     httpOnly: true,
+    secure: false,
   },
+  name: 'session-cookie',
 }));
 
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true })); //true 면 qs, false 면 querystring 모듈 쓴다.
-
-app.use('/', indexRouter);
-app.use('/user', userRouter);
-
-app.use('/', (req, res, next) => {
-  req.session.data = 'zerocho비번';
-}); 
+const multer = require('multer');
+const fs = require('fs');
 
 try {
   fs.readdirSync('uploads');
 } catch (error) {
-  console.error('uploads 폴더가 없어 uploads 폴더를 생성 한다.');
+  console.error('uploads 폴더가 없어 uploads 폴더를 생성합니다.');
   fs.mkdirSync('uploads');
 }
-
 const upload = multer({
   storage: multer.diskStorage({
     destination(req, file, done) {
@@ -57,49 +46,25 @@ const upload = multer({
   }),
   limits: { fileSize: 5 * 1024 * 1024 },
 });
-
-app.post('/upload', upload.fields([{name: 'image1', name: 'image2', name: 'image3'}]), (req, res) => { 
+app.get('/upload', (req, res) => {
+  res.sendFile(path.join(__dirname, 'multipart.html'));
+});
+app.post('/upload', upload.single('image'), (req, res) => {
   console.log(req.file);
   res.send('ok');
 });
 
-app.get('/', (req, res) => {
-  // req.session.id = 'hello'; // 요청한 사람만 id hello가 된다.
-  req.session.data = 'zerocho비번';
-  res.sendFile(path.join(__dirname, 'index.html'));
+app.get('/', (req, res, next) => {
+  console.log('GET / 요청에서만 실행됩니다.');
+  next();
+}, (req, res) => {
+  throw new Error('에러는 에러 처리 미들웨어로 갑니다.')
 });
-
-
-app.get('/category/:name', (req, res) => { // : => 와일드 카드 
-  res.send(`hello ${req.params.name}`);
-});
-
-app.post('/', (req, res) => {
-  res.send('hello express');
-});
-
-app.get('/about', (req, res) => {
-  res.send('hello express');
-});
-
 app.use((err, req, res, next) => {
   console.error(err);
-  res.send('에러지롱~')
-})
-
-// const a = (err, req, res, next) => { // const a, const b 다른 함수 console.log 다르게 찍힌다.
-//   console.error(err);
-// }
-// const b = (err, req, res) => {
-//   console.error(err);
-// }
-
-// console.log(a.length, b.length);
-
-app.listen(app.get('port'), () => {
-  console.log('익스프레스 서버 실행');
+  res.status(500).send(err.message);
 });
 
-// 1. 범위가 넓은 라우터들은 밑에 넣는다.
-// 2. 에러 미들웨어 4개 다쓴다.
-// 3. 한 라우터에서 여러번 res.send, res.json 쓰면 cannot set headers after they are sent to the client라는 오류 뜸
+app.listen(app.get('port'), () => {
+  console.log(app.get('port'), '번 포트에서 대기 중');
+});

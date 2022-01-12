293aa78736f15ab5e5e1c1311f5d768d79aea364
jungho shin <wjdghtls11@gmail.com>
2022-01-12 14:41:00 +0900

    mongoDB Prac

---
+++ mongoDB/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const User = require('../schemas/user');
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+  try {
+    const users = await User.find({});
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+module.exports = router;

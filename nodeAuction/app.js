48c1ec537fec972cd5949c4e113b80bdb2112a21
jungho shin <wjdghtls11@gmail.com>
2022-02-14 16:40:14 +0900

    Add: 실시간 경매기능 구현

--- nodeAuction/app.js
+++ nodeAuction/app.js
@@ -11,7 +11,7 @@ dotenv.config();
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth');
 const { sequelize } = require('./models');
-const passportConfig = require('./passport');
+const passportConfig = require('./passport'); 
 const webSocket = require('./socket');
 const sse = require('./sse');
 
--- nodeAuction/socket.js
+++ nodeAuction/socket.js
@@ -3,11 +3,11 @@ const SocketIO = require('socket.io');
 module.exports = (server, app) => {
   const io = SocketIO(server, { path: '/socket.io' });
   app.set('io', io);
-  io.on('connection', (server) => {
+  io.on('connection', (socket) => {
     const req = socket.request;
-    const { header: { referer } } = req;
+    const { headers: { referer } } = req;
     const roomId = referer.split('/')[referer.split('/').length - 1];
-    socket,join(roomId);
+    socket.join(roomId);
     socket.on('disconnect', () => {
       socket.leave(roomId);
     });
